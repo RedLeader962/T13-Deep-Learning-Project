41a06e85bfdb02a0691212ba76aa9b5ef485c2b5
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-04-11 16:45:31 -0400

    T13PRO-62 | DocString setup example

--- codebase/heollo_world.py
+++ codebase/heollo_world.py
@@ -1,11 +1,19 @@
 # coding=utf-8
 
-def hllow():
-    print("hellow world")
-    pass
-    return None
+def myCoolfunction(myCoolmessage: str, show: bool = False) -> str:
+    """
+    Produit un 'message de bienvenue' sur mesure et normaliser.
+
+    :param myCoolmessage: Le message à ajouter
+    :param show: Option pour imprimer le message dans la console
+    :return: Le message
+    """
+    msg = f"hellow world and {myCoolmessage}"
+    if show:
+        print(msg)
+    return msg
 
 
-def hhltest() :
+def hhltest():
     pass
     return None
