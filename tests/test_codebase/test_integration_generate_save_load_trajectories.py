7a4078599c7e6cea4a55b46c9cbdd671959dcefb
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-04-24 18:22:59 -0400

    T13PRO-98 | implement integration test related to (T13PRO-95 CodeReview | PR #9 Modif_get_trajectorie)

---
+++ tests/test_codebase/test_integration_generate_save_load_trajectories.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+import pytest
+import os
+
+def test_rudder_generate_trajectories_PASS():
+    import gym
+    import ppo
+    import rudder
+    from Script_generate_save_load_trajectories import PpoExperimentSpec
+    from Script_generate_save_load_trajectories import main as rudder_main
+
+    print(f"\n››› CWD: {os.getcwd()}")
+
+    test_spec = PpoExperimentSpec(
+        steps_by_epoch=100,
+        n_epoches=5,
+        hidden_dim=16,
+        n_hidden_layers=1,
+        device="cpu",
+        show_plot=False,
+        n_trajectory_per_policy=1)
+
+    rudder_main(test_spec)
+
+    # assert data['E_average_return'][-1] == 500.0
--- tests/test_codebase/test_script_run.py
+++ tests/test_codebase/test_script_run.py
@@ -20,3 +20,59 @@ def test_command_line_invocation_script_run_rudder_example_PASS():
 
     # Note: exit(0) <==> clean exit without any errors/problems
     assert 0 == out, "Module invocated from command line exited with error {}".format(out)
+
+
+# @pytest.mark.skip(reason="Mute for now")
+def test_command_line_invocation_Script_generate_save_load_trajectories_PASS():
+    from os import system
+
+    out = system("python -m Script_generate_save_load_trajectories --testSpec")
+
+    # Note: exit(0) <==> clean exit without any errors/problems
+    assert 0 == out, command_line_test_error_msg(out)
+
+# @pytest.mark.skip(reason="Mute for now")
+def test_command_line_invocation_Script_run_LSTM_PASS():
+    from os import system
+
+    out = system("python -m Script_run_LSTM --testSpec")
+
+    # Note: exit(0) <==> clean exit without any errors/problems
+    assert 0 == out, command_line_test_error_msg(out)
+
+# @pytest.mark.skip(reason="Mute for now")
+def test_command_line_invocation_Script_run_LSTM_to_LSTMCell_PASS():
+    from os import system
+
+    out = system("python -m Script_run_LSTM_to_LSTMCell --testSpec")
+
+    # Note: exit(0) <==> clean exit without any errors/problems
+    assert 0 == out, command_line_test_error_msg(out)
+
+# @pytest.mark.skip(reason="Mute for now")
+def test_command_line_invocation_Script_run_LSTMCell_PASS():
+    from os import system
+
+    out = system("python -m Script_run_LSTMCell --testSpec")
+
+    # Note: exit(0) <==> clean exit without any errors/problems
+    assert 0 == out, command_line_test_error_msg(out)
+
+# @pytest.mark.skip(reason="Mute for now")
+def test_command_line_invocation_Script_run_LSTMCell_PASS():
+    from os import system
+
+    out = system("python -m Script_run_LSTMCell --testSpec")
+
+    # Note: exit(0) <==> clean exit without any errors/problems
+    assert 0 == out, command_line_test_error_msg(out)
+
+# @pytest.mark.skip(reason="Mute for now")
+def test_command_line_invocation_Script_save_load_LSTM_PASS():
+    from os import system
+
+    out = system("python -m Script_save_load_LSTM --testSpec")
+
+    # Note: exit(0) <==> clean exit without any errors/problems
+    assert 0 == out, command_line_test_error_msg(out)
+
