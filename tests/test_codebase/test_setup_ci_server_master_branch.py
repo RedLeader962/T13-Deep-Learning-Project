fe4e8ddc04e0a20774a032b610336b45267e608d
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-04-26 00:55:59 -0400

    mod to PR

--- tests/test_codebase/test_setup_ci_server_master_branch.py
+++ tests/test_codebase/test_setup_ci_server_master_branch.py
@@ -7,3 +7,4 @@ pytestmark = pytest.mark.automated_test
 # @pytest.mark.skip(reason="Mute for now")
 def test_server_setup_ci_deployement_master_branch_PASS():
     assert True
+    # test remove form teamcity trigger›››  +:*/head
