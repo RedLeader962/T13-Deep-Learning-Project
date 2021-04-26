eaae3557eadfaf81ecd0308298b05574c75e0714
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-04-26 00:41:04 -0400

    T13PRO-109 | master branch mod gitignore

--- tests/test_codebase/test_setup_ci_server_master_branch.py
+++ tests/test_codebase/test_setup_ci_server_master_branch.py
@@ -4,10 +4,6 @@ import pytest
 
 pytestmark = pytest.mark.automated_test
 
-# def test_fail():
-#    raise AssertionError
-
-
 # @pytest.mark.skip(reason="Mute for now")
 def test_server_setup_ci_deployement_master_branch_PASS():
     assert True
