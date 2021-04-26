c2774207b2bc84b33fc40b6902c5456dfb9161a9
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-04-26 18:12:44 -0400

    T13PRO-95 CodeReview | merge command line integration test ‹‹‹ T13PRO-98
    Command line trigered integration test implemented in task (T13PRO-98 Refactor | `script_.*.py` as integration test). Those test are automaticaly muted when executed on the CI server.

--- tests/test_codebase/test_script_run.py
+++ tests/test_codebase/test_script_run.py
@@ -7,8 +7,10 @@ pytestmark = pytest.mark.automated_test
 
 is_run_on_TeamCity_CI_server = is_run_on_a_teamcity_continuous_integration_server()
 
+
 def command_line_test_error_msg(out):
     return "Module invocated from command line exited with error {}".format(out)
+
 
 @pytest.mark.skipif(is_run_on_TeamCity_CI_server, reason="Mute for now")
 def test_command_line_invocation_script_run_ppo_PASS():
@@ -23,7 +25,6 @@ def test_command_line_invocation_script_run_ppo_PASS():
     assert 0 == out, command_line_test_error_msg(out)
 
 
-
 @pytest.mark.skipif(is_run_on_TeamCity_CI_server, reason="Mute for now")
 def test_command_line_invocation_script_run_rudder_example_PASS():
     from os import system
@@ -36,57 +37,61 @@ def test_command_line_invocation_script_run_rudder_example_PASS():
     assert 0 == out, command_line_test_error_msg(out)
 
 
-# @pytest.mark.skip(reason="Mute for now")
+@pytest.mark.skipif(is_run_on_TeamCity_CI_server, reason="Mute for now")
 def test_command_line_invocation_Script_generate_save_load_trajectories_PASS():
     from os import system
 
-    out = system("python -m Script_generate_save_load_trajectories --testSpec")
+    out = system("python -m script.Script_generate_save_load_trajectories --testSpec")
 
     # Note: exit(0) <==> clean exit without any errors/problems
     assert 0 == out, command_line_test_error_msg(out)
 
-# @pytest.mark.skip(reason="Mute for now")
+
+@pytest.mark.skipif(is_run_on_TeamCity_CI_server, reason="Mute for now")
 def test_command_line_invocation_Script_run_LSTM_PASS():
     from os import system
 
-    out = system("python -m Script_run_LSTM --testSpec")
+    out = system("python -m script.Script_run_LSTM --testSpec")
 
     # Note: exit(0) <==> clean exit without any errors/problems
     assert 0 == out, command_line_test_error_msg(out)
 
-# @pytest.mark.skip(reason="Mute for now")
+
+@pytest.mark.skipif(is_run_on_TeamCity_CI_server, reason="Mute for now")
 def test_command_line_invocation_Script_run_LSTM_to_LSTMCell_PASS():
     from os import system
 
-    out = system("python -m Script_run_LSTM_to_LSTMCell --testSpec")
+    out = system("python -m script.Script_run_LSTM_to_LSTMCell --testSpec")
 
     # Note: exit(0) <==> clean exit without any errors/problems
     assert 0 == out, command_line_test_error_msg(out)
 
-# @pytest.mark.skip(reason="Mute for now")
+
+@pytest.mark.skipif(is_run_on_TeamCity_CI_server, reason="Mute for now")
 def test_command_line_invocation_Script_run_LSTMCell_PASS():
     from os import system
 
-    out = system("python -m Script_run_LSTMCell --testSpec")
+    out = system("python -m script.Script_run_LSTMCell --testSpec")
 
     # Note: exit(0) <==> clean exit without any errors/problems
     assert 0 == out, command_line_test_error_msg(out)
 
-# @pytest.mark.skip(reason="Mute for now")
+
+@pytest.mark.skipif(is_run_on_TeamCity_CI_server, reason="Mute for now")
 def test_command_line_invocation_Script_run_LSTMCell_PASS():
     from os import system
 
-    out = system("python -m Script_run_LSTMCell --testSpec")
+    out = system("python -m script.Script_run_LSTMCell --testSpec")
 
     # Note: exit(0) <==> clean exit without any errors/problems
     assert 0 == out, command_line_test_error_msg(out)
 
-# @pytest.mark.skip(reason="Mute for now")
+
+@pytest.mark.skipif(is_run_on_TeamCity_CI_server, reason="Mute for now")
 def test_command_line_invocation_Script_save_load_LSTM_PASS():
     from os import system
 
-    out = system("python -m Script_save_load_LSTM --testSpec")
+    out = system("python -m script.Script_save_load_LSTM --testSpec")
 
     # Note: exit(0) <==> clean exit without any errors/problems
     assert 0 == out, command_line_test_error_msg(out)
-
