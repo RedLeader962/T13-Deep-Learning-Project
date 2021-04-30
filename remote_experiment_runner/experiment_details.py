21ad355e4b7bee24632856451a4bb7d439b46193
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-04-30 04:27:43 -0400

    T13PRO-146 | hparam mod

--- remote_experiment_runner/experiment_details.py
+++ remote_experiment_runner/experiment_details.py
@@ -10,10 +10,9 @@ colab_specs = RudderLstmParameterSearchMap(
     env_n_trajectories=lambda: random.choice([3000, 4000, 5000, ]),
     env_perct_optimal=lambda: random.choice([0.3, 0.4, 0.5, 0.5, 0.5, 0.6, 0.7, ]),
     env_rew_factor=lambda: random.choice([1.0, 1.0, 0.1, 0.01, ]),
-    n_epoches=lambda: random.choice([20, 40, 60, 80, 100, 140]),
+    n_epoches=lambda: random.choice([20, 40, 60, 80, 100, 140, 200, 300, 400, 600]),
     optimizer_weight_decay=lambda: random.choice([1e-1, 1e-2, 1e-3]),
-    optimizer_lr=1e-3,
+    optimizer_lr=lambda: random.choice([1e-1, 1e-2, 1e-3]),
     show_plot=True,
     seed=42,
-    # seed=None,
     )
